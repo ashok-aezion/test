3251f54e15599c2d9abde56bef7d31116a7f4494
ashokkumar <ashok-aezion>
2017-08-31 14:57:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+//import { RouterOutlet, RouterLink  } from '@angular/router';
+declare var device;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+    device:any;
+    ngOnInit() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+           alert(device.platform);
+        }
+    }
+    title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes, RouterOutlet, RouterLink} from '@angular/router';
+import { NgModule } from '@angular/core';   
+import { InputTextModule, ButtonModule, DataTableModule, DialogModule }  from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+//  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule,
+    InputTextModule, 
+    DialogModule,
+    ButtonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
